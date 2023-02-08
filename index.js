54f7918df07665099d2e8208d9731fe06d109b33
AlexxDv <alexxdimov@gmail.com>
2023-02-08 18:50:46 +0100

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
+const mongoose = require("mongoose");
 
 const routes = require("./routers");
 
@@ -14,8 +15,12 @@ app.engine(
 
 app.set("view engine", "hbs");
 
-app.use("/static", express.static("public"));
+app.use("/static", express.static("public")); // описваме от къде да вземе статичните файлове
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
+
+//TODO: change DB name after /
+mongoose.set('strictQuery', false);
+mongoose.connect("mongodb://127.0.0.1:27017/crypto");
 
 app.listen(5000, () => console.log("<<--Server listening on port 5000-->"));
