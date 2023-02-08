3e1447a96da934f494340207d756b2a73d2f61de
AlexxDv <alexxdimov@gmail.com>
2023-02-08 18:57:49 +0100

    user.js Added

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const { Schema, model, Error } = require("mongoose");
+
+const userShema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    virtuals: {
+      repeatPassword: {
+        set(value) {
+          if (this.password !== value) {
+            throw new Error("Password does not match");
+          }
+        },
+      },
+    },
+  }
+);
+
+const User = model(User, userShema);
+
+module.exports = User;
